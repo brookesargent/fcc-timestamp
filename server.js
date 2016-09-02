9e88ce022aacba735b600e3c62593ab1851aeb89
Brooke Sargent <brookesargent01@gmail.com>
2016-09-02 01:33:32 +0000

    remove body-parser and extra comments

--- server.js
+++ server.js
@@ -1,23 +1,10 @@
-//user stories:
-    //-can pass a string as a parameter, and it will check
-    // to see whether that string contains a unix timestamp OR a natural
-    // language date
-    
-    //-if it does, it returns both the unix timestamp and the natural 
-    // language form of the date
-    
-    //-if it does not contain a date or unix timestamp, it returns
-    // null for those properties
-    
 // grab the packages we need
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 8080;
-var bodyParser = require('body-parser');
 var moment = require('moment');
 
 // routes will go here
-//use /:timestamp as the parameter when ready
 app.get('/:timestamp', function(req, res) {
   var date = req.params.timestamp;
   var naturalDate;
